70e5943f4e73bce632df53944bc6a06b948670ee
samuel guerrero <samuel040902@hotmail.com>
2022-05-23 17:35:24 -0500

    Add files via upload

---
+++ parcial2_b/src/main/java/model/Msg.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Msg {
+    private String type;
+    private String msg;
+
+    public Msg(String type, String msg) {
+        this.type = type;
+        this.msg = msg;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
